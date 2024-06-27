3d48a38e572637fcb2eeb51a1ef94f85f232ce8c
Eirado <GABEIRADO2@HOTMAIL.COm>
2024-06-27 14:26:35 -0300

    codeAndName / codeAndQuotation

--- Nano07Challenge/Nano07Challenge/ViewModels/ViewModel.swift
+++ Nano07Challenge/Nano07Challenge/ViewModels/ViewModel.swift
@@ -7,24 +7,47 @@
 
 import Foundation
 class ViewModel: ObservableObject{
+    
     let apiManager = APIManager()
     
     let codeAndName = "https://raw.githubusercontent.com/Banking-iOS/mock-interview/main/api/list.json"
     let codeAndQuotation = "https://raw.githubusercontent.com/Banking-iOS/mock-interview/main/api/live.json"
     
-    var coins: Coins?
-    var currenciesName: CurrenciesName?
+    @Published var coins: [String : Float]?
+    @Published var currenciesNames: [String : String]?
     
     init(){
-        fetch()
+        fetchCurrencies()
+        fetchCoins()
     }
     
-    func fetch(){
+    func fetchCurrencies(){
         Task {
-            let result = await apiManager.fetch(httpLink: codeAndName, object: currenciesName)
+            let result = await apiManager.fetch(httpLink: codeAndName, object: CurrenciesName(currencies: ["":""]))
             switch result {
             case .success(let decodedObject):
-                print("Successfully fetched and decoded: \(String(describing: decodedObject))")
+                self.currenciesNames = decodedObject.currencies
+                print(decodedObject.currencies)
+            case .failure(let error):
+                switch error {
+                case .invalidURL:
+                    print("Invalid URL.")
+                case .networkError(let networkError):
+                    print("Network error: \(networkError.localizedDescription)")
+                case .decodingError(let decodingError):
+                    print("Decoding error: \(decodingError.localizedDescription)")
+                }
+            }
+        }
+    }
+    
+    func fetchCoins(){
+        Task {
+            let result = await apiManager.fetch(httpLink: codeAndName, object: Coins(quotes: ["":0]))
+            switch result {
+            case .success(let decodedObject):
+                self.coins = decodedObject.quotes
+                print(decodedObject.quotes)
             case .failure(let error):
                 switch error {
                 case .invalidURL:
